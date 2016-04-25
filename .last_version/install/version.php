7d065a51a23f5409e76d1483a29bc2d9b97b4624
Aleksey Zadoiniy <a@coffeediz.ru>
2016-04-25 16:22:10 +0400

    v.0.0.1

---
+++ .last_version/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+    "VERSION" => "0.0.1",
+    "VERSION_DATE" => "2016-04-15 14:20:00"
+);
+?>
---
+++ .last_version/lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?
+$MESS["COFFEEDIZ_INSTALL_NAME_GIT"] = "GIT.PHP";
+$MESS["COFFEEDIZ_INSTALL_DESCRIPTION_GIT"] = "Модуль для работы с GIT из PHP";
+$MESS["COFFEEDIZ_INSTALL_TITLE"] = "Установка модуля";
+$MESS["COFFEEDIZ_UNINSTALL_TITLE"] = "Удаление модуля";
+$MESS["SPER_PARTNER_GIT"] = "Задойный Алексей";
+$MESS["PARTNER_URI_GIT"] = "http://кофедизайн.рф/";
+?>
